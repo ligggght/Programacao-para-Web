d413c0c08646b45c73fcb7fd3a26e32d6f3a5884
Marco <marcoantonio.machadodearruda@gmail.com>
2025-09-16 17:16:45 -0300

    alteracao no next.config para que o servidor suporte

--- PW/mastermind/next.config.ts
+++ PW/mastermind/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/AA/aa7', // o caminho do seu projeto no servidor
+  assetPrefix: '/AA/aa7/', // prefixo para assets (_next/*)
 };
 
 export default nextConfig;
